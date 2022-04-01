f4eb3ac9d42b422b53821b176b981024297c9901
Maorio <maorar1@ac.sce.ac.il>
2022-04-01 17:34:46 +0300

    RGB awesome gamer SWAG
    Awesome

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,15 @@ import { Grid, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import MainTheme from "../themes/MainTheme";
 import { CssBaseline } from "@mui/material";
+import { keyframes } from '@emotion/react'
+
+
+const RGB= keyframes`
+    0% { color: red; }
+  33% { color: blue; }
+  66% { color: green; }
+  100% { color: red; }
+`
 
 const HomePage = () => {
   return (
@@ -17,7 +26,7 @@ const HomePage = () => {
           }}
         >
           <CssBaseline />
-          <Typography variant='h1'>HOME PAGE</Typography>
+          <Typography variant='h1' sx={{animation: `${RGB} 2.5s infinite`}} >HOME PAGE</Typography>
         </Grid>
       </ThemeProvider>
     </>
