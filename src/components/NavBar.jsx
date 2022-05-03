e8bc8405098c69d37cd62b14b12b5edd0882923c
neriya zudi <neriyazudi@gmail.com>
2022-05-03 14:52:08 +0300

    nav bar costum

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope */
 // import React, { useState } from "react";
-import MainNavBar from './Logout/MainNavBar';
-//import CustomerNavBar from './Login/CustomerNavBar';
-import { useSelector } from 'react-redux';
-import { getUser } from '../store/State';
-import ManagerNavBar from './Login/ManagerNavBar';
+import MainNavBar from "./Logout/MainNavBar";
+import CustomerNavBar from "./Login/CustomerNavBar";
+import { useSelector } from "react-redux";
+import { getUser } from "../store/State";
+import ManagerNavBar from "./Login/ManagerNavBar";
 
 const NavBar = () => {
   const state = useSelector((s) => s);
   // const dispatch=useDispatch();
+  let navBarType;
 
   const user = getUser(state);
   if (user !== undefined) {
     console.log(user.first_name);
   } else {
-    console.log('no log in');
+    console.log("no log in");
+  }
+  if (user !== undefined) {
+    navBarType = user.isAdmin ? <ManagerNavBar /> : <CustomerNavBar />;
+  } else {
+    navBarType = <MainNavBar />;
   }
 
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <>{user !== undefined ? <ManagerNavBar /> : <MainNavBar />}</>;
+  return <>{navBarType}</>;
 };
 
 export default NavBar;
